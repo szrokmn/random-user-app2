426203ef23a9102b159c341732626b65760f4d1b
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-22 17:11:33 +0300

    continue

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState, useEffcet} from 'react'
 import cardStyle from "./Card.module.scss"
-
+import axios from "axios"
+const url ="http://randomuser.me/api/"
 const Card = () => {
     const [personInfo, setPersonInfo] = useState({})
+    const [loading, setLoading] = useState(false)
+    const [hoverData, setHoverData] = useState({
+        text1:"",
+        text2:""
+    })
+
+    const apiFetcher = async()=> {
+        setLoading(true)
+    try {       
+            const response = await axios(url);  
+            const person = response.data.esults[0];
+            const {email, gender, phone, dob:{age}, picture:{large:image}, name:{title, first, last}, location:{country}, } = person;
+    } catch (error) {
+        console.log(error)
+    }    
+    setLoading(false)    
+    }
+
+    useEffect(() => {
+      apiFetcher()
+    }, [])
+    
   return (
     <div className={cardStyle.card}>
       <nav className={appStyle.navbar}>        
