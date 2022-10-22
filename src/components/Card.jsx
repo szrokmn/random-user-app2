dd4bcffe16e1f218c9e0cef47d4a595c780e4619
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-22 16:55:28 +0300

    continue

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import cardStyle from "./Card.module.scss"
 
 const Card = () => {
+    const [personInfo, setPersonInfo] = useState({})
   return (
-    <div>
-      Hrlloo
+    <div className={cardStyle.card}>
+      <nav className={appStyle.navbar}>        
+      </nav>
+      <img className={appStyle.cwImage} src="" alt="" />
+      <div></div>
+      <div></div>
     </div>
   )
 }
